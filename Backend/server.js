800c423e1bf3b4ce86c184044356fb7539d6935b
19Peyton19 <124940998+19Peyton19@users.noreply.github.com>
2025-08-25 09:15:44 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ function log(level, message, data = null) {
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5501', 'http://localhost:3000', 'https://kanjitenstudy.netlify.app/'],
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:5501', 'http://localhost:3000', 'https://kanjitenstudy.netlify.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
