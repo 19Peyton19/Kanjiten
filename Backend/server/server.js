1a12995afcf4118c298c46aa716769cc1f37651f
19Peyton19 <124940998+19Peyton19@users.noreply.github.com>
2025-08-25 07:42:28 -0400

    Update server.js

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 
 // Initialize Supabase client
 const supabaseUrl = process.env.SUPABASE_URL;
